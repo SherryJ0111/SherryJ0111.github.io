e8c7baa01a39696ad7263099be4f7bbe26b55f8d
SherryJ0111 <scholar_best@hotmail.com>
2018-01-24 12:27:22 +0800

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -66,7 +66,7 @@ HTMLActuator.prototype.addTile = function (tile) {
 //Adaption Start
   if(window.my_list){
     inner.textContent = my_list[tile.value] || tile.value;
-	inner.style.fontSize = (2)+ 'px';
+	inner.style.fontSize = (1/inner.textContent.length * 80)+ 'px';
     inner.style.fontFamily = '黑体';
   }
 //Adaption Close
